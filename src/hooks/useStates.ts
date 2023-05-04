fa48f892c20c47de768e3c249a0cc53e51034f64
Jake Simon <Jaketylersimon@gmail.com>
2023-05-03 18:15:19 -0700

    init

--- src/hooks/useStates.ts
+++ src/hooks/useStates.ts
@@ -1,1 +1,47 @@
+import { useCallback, useEffect, useState } from 'react'
 
+export interface StateRow {
+    id: string
+    key: string
+    name: string
+    slug: string
+}
+
+const useStates = () => {
+    const [results, setResults] = useState<StateRow[]>()
+    const [nameSearchString, setNameSearchString] = useState<string>('')
+    const clearFilter = useCallback(() => {
+        setNameSearchString('')
+    }, [])
+    const search = useCallback((searchString: string) => {
+        setNameSearchString(searchString)
+    }, [])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(
+                'https://datausa.io/api/searchLegacy?dimension=Geography&hierarchy=State&limit=50000'
+            )
+            const data = await response.json()
+            setResults(data.results)
+        }
+        fetchData()
+    }, [])
+
+    if (nameSearchString.trim() !== '' && results) {
+        return {
+            results: results?.filter((result) =>
+                result.name
+                    .toLowerCase()
+                    .startsWith(nameSearchString.toLowerCase())
+            ),
+            clearFilter,
+            search,
+            nameFilter: nameSearchString,
+        }
+    }
+
+    return { results, clearFilter, search, nameFilter: nameSearchString }
+}
+
+export default useStates
